1ba7bd4d85f964fcaea1b6cad5ec2d0d90f25a9c
mattre <mat@quorum.at>
2016-12-27 22:50:02 +0100

    Updates

---
+++ README.md
@@ -0,0 +1,6 @@
+Seasonal Globe
+
+Data from NEO Obersvations
+
+Convert images:
+`mogrify -path data/temperature -resize 2048x1024 *jpg`
---
+++ index.js
@@ -0,0 +1,12 @@
+require('./lib/aframe.min.js');
+require('aframe-animation-component');
+require('aframe-layout-component');
+require('aframe-template-component');
+require('aframe-bmfont-text-component');
+require('aframe-look-at-component');
+require('aframe-event-set-component');
+require('mesh-ui');
+
+
+require('./components/dynamic-globe');
+require('./components/set-attribute');
